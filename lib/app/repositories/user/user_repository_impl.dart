29d8f497dfa3a965424b9f754c193acf9b4b314c
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-14 11:02:33 -0300

    configuração de conexão com ogin com google firebase

--- lib/app/repositories/user/user_repository_impl.dart
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -1,3 +1,46 @@
-class UserRepositoryImpl {
-  
+import 'package:diet_pdf/app/exception/auth_exception.dart';
+import 'package:diet_pdf/app/repositories/user/user_reopository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserRepositoryImpl implements UserReopository {
+  FirebaseAuth _firebaseAuth;
+
+  UserRepositoryImpl({required FirebaseAuth firebaseAuth})
+      : _firebaseAuth = firebaseAuth;
+
+  @override
+  Future<User?> googleLogin() async {
+    List<String>? loginMethods;
+
+    try {
+      final googleSignIn = GoogleSignIn();
+      final googleUser = await googleSignIn.signIn();
+
+      if (googleUser != null) {
+        final googleAuth = await googleUser.authentication;
+        final firebaseCredential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        var userCredential =
+            await _firebaseAuth.signInWithCredential(firebaseCredential);
+
+        return userCredential.user;
+      }
+    } on FirebaseAuthException catch (e, s) {
+      print(e);
+      print(s);
+
+      if (e.code == 'account-exists-with-different-credencial') {
+        throw AuthException(message: '''
+        Login inválido, você se registrou no TodoList com os seguintes provedores:
+        ${loginMethods!.join(',')}
+        ''');
+      } else {
+        throw AuthException(message: 'Erro ao realizar login');
+      }
+    }
+  }
 }
